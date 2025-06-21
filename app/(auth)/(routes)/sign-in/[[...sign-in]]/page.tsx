fe62221d59caae11994037ecfe04649a53eea57f
Pushkar kamble <pushkarkamble23@gmail.com>
2025-06-21 08:54:46 +0530

    minor fixes

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,14 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+  return (
+    <>
+      <SignIn />
+      <div className="text-center text-sm text-gray-500">
+        dummy credentials: <br />
+        email: test@test.com <br />
+        password: test123
+      </div>
+    </>
+  );
 }
